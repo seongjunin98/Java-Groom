72bd4babacbbe25c4b4c0f3ddc6d0570d92ca395
SeongJun98 <seongjunin98@gmail.com>
2025-03-26 16:59:12 +0900

    Study
    exception

---
+++ chap13-exception-source/src/main/java/com/seongjun/section02/userexception/exception/MoneyNegativeException.java
@@ -0,0 +1,8 @@
+package com.seongjun.section02.userexception.exception;
+
+public class MoneyNegativeException extends NegativeException {
+
+    public MoneyNegativeException(String message) {
+        super(message);
+    }
+}
---
+++ chap13-exception-source/src/main/java/com/seongjun/section02/userexception/exception/PriceNegativeException.java
@@ -0,0 +1,8 @@
+package com.seongjun.section02.userexception.exception;
+
+public class PriceNegativeException extends NegativeException {
+
+    public PriceNegativeException(String message) {
+        super(message);
+    }
+}
